37a8e3ad06072dc18fe02a7cec655b1e65e1c756
Rakesh Sharma <raks437@gmail.com>
2023-04-13 19:11:38 +0530

    init;

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import lit from "@astrojs/lit";
+import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [lit(), tailwind()],
+  output: "server",
+  adapter: netlify()
+});
